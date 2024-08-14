048bfd2db8addafff896ce1f2b588e6b83cebced
Muhammad Ismail <m.ismailusman.477@gmail.com>
2024-08-14 22:11:38 +0500

    Add responsive footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./Footer.css"
+function Footer() {
+   return (
+      <footer>
+         <p>Copyright &copy; 2024 Prettierrc Generator.</p>
+         <div>
+            <span>Powered By: </span><a href="#">Muhammad Ismail</a>
+         </div>
+      </footer>
+   )
+}
+
+export default Footer
