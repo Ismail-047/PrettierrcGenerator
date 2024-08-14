048109b907780ac568a154addf8cd3b8a258e524
Muhammad Ismail <m.ismailusman.477@gmail.com>
2024-08-14 21:33:40 +0500

    Add responsive Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+   return (
+      <header>
+         <h1>Prettierrc Config Generator</h1>
+      </header>
+   )
+}
+
+export default Header
