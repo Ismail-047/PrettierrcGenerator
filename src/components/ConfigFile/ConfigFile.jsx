652a58574f38429293488a73d147d626fc922e51
Muhammad Ismail <m.ismailusman.477@gmail.com>
2024-08-29 17:29:21 +0500

    Fix a Bug

--- src/components/ConfigFile/ConfigFile.jsx
+++ src/components/ConfigFile/ConfigFile.jsx
@@ -64,98 +64,98 @@ function ConfigFile({
                   <div className="keyValueContainer">
                      {semicolons && (
                         <>
-                           <span className='keys'>semi:</span>
+                           <span className='keys'>"semi":</span>
                            <span className='values boolvalues'>{semicolons}</span>
                            <br />
                         </>
                      )}
                      {tabs && (
                         <>
-                           <span className='keys'>tabs:</span>
+                           <span className='keys'>"useTabs":</span>
                            <span className='values boolvalues'>{tabs}</span>
                            <br />
                         </>
                      )}
                      {quotes && (
                         <>
-                           <span className='keys'>quotes:</span>
+                           <span className='keys'>"singleQuote":</span>
                            <span className='values boolvalues'>{quotes}</span>
                            <br />
                         </>
                      )}
                      {rangeEnd && (
                         <>
-                           <span className='keys'>rangeEnd:</span>
+                           <span className='keys'>"rangeEnd":</span>
                            <span className='values numberValues'>{rangeEnd}</span>
                            <br />
                         </>
                      )}
                      {tabWidth && (
                         <>
-                           <span className='keys'>tabWidth:</span>
+                           <span className='keys'>"tabWidth":</span>
                            <span className='values numberValues'>{tabWidth}</span>
                            <br />
                         </>
                      )}
                      {proseWrap && (
                         <>
-                           <span className='keys'>proseWrap:</span>
+                           <span className='keys'>"proseWrap":</span>
                            <span className='values stringValues'>{`"${proseWrap}"`}</span>
                            <br />
                         </>
                      )}
                      {rangeStart && (
                         <>
-                           <span className='keys'>rangeStart:</span>
+                           <span className='keys'>"rangeStart":</span>
                            <span className='values numberValues'>{rangeStart}</span>
                            <br />
                         </>
                      )}
                      {printWidth && (
                         <>
-                           <span className='keys'>printWidth:</span>
+                           <span className='keys'>"printWidth":</span>
                            <span className='values numberValues'>{printWidth}</span>
                            <br />
                         </>
                      )}
                      {jsxBrackets && (
                         <>
-                           <span className='keys'>jsxBrackets:</span>
+                           <span className='keys'>"jsxBracketSameLine":</span>
                            <span className='values boolvalues'>{jsxBrackets}</span>
                            <br />
                         </>
                      )}
                      {insertPragma && (
                         <>
-                           <span className='keys'>insertPragma:</span>
+                           <span className='keys'>"insertPragma":</span>
                            <span className='values boolvalues'>{insertPragma}</span>
                            <br />
                         </>
                      )}
                      {requirePragma && (
                         <>
-                           <span className='keys'>requirePragma:</span>
+                           <span className='keys'>"requirePragma":</span>
                            <span className='values boolvalues'>{requirePragma}</span>
                            <br />
                         </>
                      )}
                      {trailingCommas && (
                         <>
-                           <span className='keys'>trailingCommas:</span>
+                           <span className='keys'>"trailingComma":</span>
                            <span className='values stringValues'>{`"${trailingCommas}"`}</span>
                            <br />
                         </>
                      )}
                      {bracketSpacing && (
                         <>
-                           <span className='keys'>bracketSpacing:</span>
+                           <span className='keys'>"bracketSpacing":</span>
                            <span className='values boolvalues'>{bracketSpacing}</span>
                            <br />
                         </>
                      )}
                      {arrowFunctionParentheses && (
                         <>
-                           <span className='keys'>arrowFunctionParentheses:</span>
+                           <span className='keys'>"arrowParens":</span>
                            <span className='values stringValues'>{`"${arrowFunctionParentheses}"`}</span>
                            <br />
                         </>
